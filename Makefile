38b5796f10b92b19f11e1868398c436c1dc93a98
89luca89 <luca.dimaio1@gmail.com>
2020-08-18 08:38:54 +0200

    add support for bastion ssh, variable provider, pool name and vm name

--- Makefile
+++ Makefile
@@ -3,13 +3,28 @@ PROJECT := $(THISDIR)
 distro ?= CFG_DISTRO_ID
 
 apply: ## applies this
-	cd terraform &&	terraform apply -auto-approve -var 'hostname=$(distro)' -var 'disk_source=~/VirtualMachines/$(distro)-terraform.qcow2'
+	cd terraform &&	terraform apply -auto-approve \
+		-var 'hostname=$(distro)' \
+		-var 'disk_source=~/VirtualMachines/$(distro)-terraform.qcow2' \
+		-var 'pool_name=base' \
+		-var 'macvtap_iface=internal' \
+		-var 'provider_uri=qemu+ssh://root@10.90.20.21/system'
 
 init: ## first time
-	cd terraform &&	terraform init -var 'hostname=$(distro)' -var 'disk_source=~/VirtualMachines/$(distro)-terraform.qcow2'
+	cd terraform &&	terraform init \
+		-var 'hostname=$(distro)' \
+		-var 'pool_name=base' \
+		-var 'macvtap_iface=internal' \
+		-var 'disk_source=~/VirtualMachines/$(distro)-terraform.qcow2' \
+		-var 'provider_uri=qemu+ssh://root@10.90.20.21/system'
 
 destroy:
-	cd terraform &&	terraform destroy -auto-approve -var 'hostname=$(distro)' -var 'disk_source=~/VirtualMachines/$(distro)-terraform.qcow2'
+	cd terraform &&	terraform destroy -auto-approve \
+		-var 'hostname=$(distro)' \
+		-var 'disk_source=~/VirtualMachines/$(distro)-terraform.qcow2' \
+		-var 'pool_name=base' \
+		-var 'macvtap_iface=internal' \
+		-var 'provider_uri=qemu+ssh://root@10.90.20.21/system'
 
 ## create public/private keypair for ssh
 create-keypair:
@@ -18,7 +33,7 @@ create-keypair:
 
 iso:
 	cd packer/$(distro) && packer build $(distro).json
-	cp -f packer/$(distro)/artifacts/qemu/packer-template-$(distro)-x86_64 ~/VirtualMachines/$(distro)-terraform.qcow2
+	cp -f packer/$(distro)/artifacts/qemu/packer-template-$(distro)-x86_64 ~/VirtualMachines/$(distro)-terraform.img
 	rm -rf packer/$(distro)/artifacts/qemu
 
 ## list make targets
--- terraform/template-vm.tf
+++ terraform/template-vm.tf
@@ -4,24 +4,26 @@
 
 # to set hostname and source file use:
 #   -var 'hostname=example' -var 'source=~/VirtualMachines/example.qcow2'
-variable "hostname" { default = "base-hostname" }
-variable "disk_source" { default = "~/VirtualMachines/image-terraform.qcow2" }
-variable "domain" { default = "lan" }
-variable "memory_mb" { default = 1024 * 4 }
 variable "cpu" { default = 2 }
-variable "macvtap_iface" { default = "enp0s31f6" }
+variable "disk_source" { default = "" }
+variable "domain" { default = "" }
+variable "hostname" { default = "" }
+variable "macvtap_iface" { default = "" }
+variable "memory_mb" { default = 1024 * 4 }
+variable "provider_uri" { default = "qemu:///system" }
+variable "pool_name" { default = "" }
 
 # instance the provider
 provider "libvirt" {
-  uri = "qemu:///system"
+  uri = var.provider_uri
 }
 
 # Base OS image to use to create a cluster of different
 # nodes
 resource "libvirt_volume" "os_image" {
-  name   = "packer-template"
+  name   = format("%s-%s", var.hostname, "terraform")
   source = pathexpand(var.disk_source)
-  pool   = "pool"
+  pool   = var.pool_name
   format = "qcow2"
 }
 
@@ -38,25 +40,25 @@ resource "libvirt_domain" "domain-terraform" {
   disk { volume_id = libvirt_volume.os_image.id }
   boot_device { dev = ["cdrom", "hd", "network"] }
 
-  filesystem {
-    source   = pathexpand("~/")
-    target   = "mnt"
-    readonly = false
-  }
+  # filesystem {
+  #   source   = pathexpand("~/")
+  #   target   = "mnt"
+  #   readonly = false
+  # }
 
   # uses static  IP
   network_interface {
     network_name   = "default"
     hostname       = var.hostname
-    addresses      = ["192.168.124.203"]
-    mac            = "AA:BB:CC:11:22:22"
+    addresses      = ["192.168.122.241"]
+    mac            = "AA:BB:CC:11:24:23"
     wait_for_lease = true
   }
 
- #  network_interface {
- #    macvtap  = var.macvtap_iface
- #    hostname = var.hostname
- #  }
+  #  network_interface {
+  #    bridge  = var.macvtap_iface
+  #    hostname = var.hostname
+  #  }
 
   # IMPORTANT
   # it will show no console otherwise
@@ -66,20 +68,24 @@ resource "libvirt_domain" "domain-terraform" {
 
   graphics {
     type        = "spice"
-    listen_type = "none"
+    listen_type = "address"
     autoport    = "true"
   }
 
   connection {
-    type     = "ssh"
-    host     = self.network_interface[0].addresses[0]
-    user     = "root"
-    password = "root"
+    type             = "ssh"
+    host             = self.network_interface[0].addresses[0]
+    user             = "root"
+    password         = "root"
+    bastion_host     = "10.90.20.21"
+    bastion_port     = "22"
+    bastion_user     = "root"
+    bastion_password = "password"
   }
 
   # Hostdev passtrought
   # provisioner "local-exec" {
-  #   command = "virsh --connect qemu:///system  attach-device ${var.hostname} --file passtrought-host.xml --live --persistent"
+  #   command = "virsh --connect ${var.provider_uri} attach-device ${var.hostname} --file passtrought-host.xml --live --persistent"
   # }
 
   # provisioner "remote-exec" {
@@ -87,7 +93,7 @@ resource "libvirt_domain" "domain-terraform" {
   # }
 
   # provisioner "remote-exec" {
-  #   inline = ["echo second"]
+  #   inline = ["nmcli con mod \"$(nmcli -t -f NAME c s  | grep -v eth0)\" ipv4.addresses 10.0.0.60/24 ipv4.dns 10.0.0.1"]
   # }
 }
 
